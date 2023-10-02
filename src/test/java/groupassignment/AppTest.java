b59f9042ca7e5ac1124d0c87888fa0c46bb390fb
lisapel <lisa.peldan@gmail.com>
2023-10-02 10:11:40 +0200

    file config

--- src/test/java/groupassignment/AppTest.java
+++ src/test/java/groupassignment/AppTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//tests
 @SpringBootTest
 class AppTest {
     @Test void appHasAGreeting() {
